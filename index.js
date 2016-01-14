6eb1ee9f23de6683300c3758acbd3ee3b3ea8f67
Jeremy Danyow <jdanyow@gmail.com>
2016-01-14 09:14:39 -0500

    test

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ var port = process.env.PORT || 5000;
 var client_id = process.env.ES_RUN_CLIENTID || 'abcdef';
 var client_secret = process.env.ES_RUN_SECRET || 'ghyjklmnopqrstuvwxyz';
 
+function addCorsHeaders(headers) {
+  headers['Access-Control-Allow-Origin'] = 'https://ecmascript.run';
+  headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS';
+  headers['Access-Control-Allow-Credentials'] = false;
+  headers['Access-Control-Max-Age'] = '86400'; // 24 hours
+  headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept';
+}
+
 function handleRequest(req, res) {
   if (req.method === 'GET' && /^\/access-token[?]/.test(req.url)) {
     var query = url.parse(req.url, true).query;
@@ -18,16 +26,15 @@ function handleRequest(req, res) {
     };
     request.post('https://github.com/login/oauth/access_token')
       .form(args)
+      .on('response', function(res) {
+        addCorsHeaders(res.headers);
+      })
       .pipe(res);
     return;
   }
   if (req.method === 'OPTIONS') {
     var headers = {};
-    headers['Access-Control-Allow-Origin'] = 'https://ecmascript.run';
-    headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS';
-    headers['Access-Control-Allow-Credentials'] = false;
-    headers['Access-Control-Max-Age'] = '86400'; // 24 hours
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept';
+    addCorsHeaders(headers);
     res.writeHead(200, headers);
     res.end();
     return;
